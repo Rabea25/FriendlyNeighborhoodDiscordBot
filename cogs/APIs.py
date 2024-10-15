c9598d0dfe30ac794de0eea3b46b6bc73a4bb57e
A. Rabe3 <a.rabe3.425@gmail.com>
2024-10-15 18:17:11 +0300

    added another timezone for prayer times command

--- cogs/APIs.py
+++ cogs/APIs.py
@@ -14,10 +14,18 @@ class APIS(commands.Cog):
 
 
     @commands.hybrid_command(name='pr', with_app_command=True, aliases=["PR", "pR", "Pr"])
-    async def pr(self, ctx):
+    async def pr(self, ctx, tz='EG'):
         location = aladhan.City("Alexandria", "EG", "Egypt")
         client = aladhan.Client(location)
         x = client.get_today_times(location)
+        if tz=='KSA' or tz=='ksa' or tz=='sa' or tz=='SA':
+            location = aladhan.City("Jeddah", "SA")
+            client = aladhan.Client(location)
+            x = client.get_today_times(location)
+        #if tz == 'KR' or tz == 'kr' or tz == 'Kr' or tz == 'kR':
+        #    location = aladhan.City("AbuDhabi", "AE")
+        #    client = aladhan.Client(location)
+        #    x = client.get_today_times(location)
         for adhan in x:
             await ctx.send("{: <15} | {: <15}".format(adhan.get_en_name(), adhan.readable_timing(show_date=False)))
 
